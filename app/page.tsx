bc0dbd1777a9474e222002711fe66571e865682d
arossman1226 <78833799+arossman1226@users.noreply.github.com>
2025-06-27 10:26:46 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
+
 import { useState } from "react";
+
 export default function LawnEstimator() {
   const [size, setSize] = useState(2000);
   const [grassHeight, setGrassHeight] = useState("normal");
